eec63a13e876fb4717b18617d0762d9f498ed12f
Pei Yi <peiyijessychen@gmail.com>
2023-03-03 19:05:37 +0800

    forked

---
+++ to-do-list/script.js
@@ -0,0 +1,48 @@
+// 初始變數
+const list = document.querySelector("#my-todo");
+const addBtn = document.querySelector("#add-btn");
+const input = document.querySelector("#new-todo");
+
+// 資料
+const todos = [
+  "Hit the gym",
+  "Read a book",
+  "Buy eggs",
+  "Organize office",
+  "Pay bills"
+];
+
+for (let todo of todos) {
+  addItem(todo);
+}
+
+// 函式
+function addItem(text) {
+  let newItem = document.createElement("li");
+  newItem.innerHTML = `
+    <label for="todo">${text}</label>
+    <i class="delete fa fa-trash"></i>
+  `;
+  list.appendChild(newItem);
+}
+
+// Create
+addBtn.addEventListener("click", function () {
+  const inputValue = input.value;
+
+  if (inputValue.length > 0) {
+    addItem(inputValue);
+  }
+});
+
+// Delete and check
+list.addEventListener("click", function (event) {
+  const target = event.target;
+
+  if (target.classList.contains("delete")) {
+    let parentElement = target.parentElement;
+    parentElement.remove();
+  } else if (target.tagName === "LABEL") {
+    target.classList.toggle("checked");
+  }
+});
