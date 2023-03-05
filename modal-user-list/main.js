95c83d699316e4d2bb90eb508e3b4dece6560b30
Pei-Yi <whitejavasparrow@gmail.com>
2023-03-05 18:22:59 +0800

    user-list update

--- modal-user-list/main.js
+++ modal-user-list/main.js
@@ -6,8 +6,48 @@ const SHOW_URL = BASE_URL + "/api/v1/users/";
 
 const dataPanel = document.querySelector("#data-panel");
 const modal = document.querySelector("#user-modal");
-const openModalBtn = document.querySelector("#modal-btn"); //
 const closeModalBtn = document.querySelector(".close-btn");
+
+const openModal = () => {
+  modal.style.display = "block";
+};
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+
+dataPanel.addEventListener("click", function (event) {
+  if (
+    event.target.matches(".user-name") ||
+    event.target.matches(".user-avatar")
+  ) {
+    const id = event.target.dataset.id; //
+
+    axios.get(SHOW_URL + id).then(function (res) {
+      const data = res.data;
+      console.log(res.data);
+      const userName = document.querySelector("#modal-user-name");
+      const userAge = document.querySelector("#modal-user-age");
+      const userEmail = document.querySelector("#modal-user-email");
+
+      userName.innerText = `${data.name} ${data.surname} (${
+        data.gender === "female" ? "She / her / Ms." : "He / his / Mr."
+      })`;
+      userAge.innerText = `Birthday: ${data.birthday} (${data.age})`;
+      userEmail.innerText = `based in ${data.region} | email: ${data.email}`;
+
+      openModal();
+    });
+  }
+});
+
+closeModalBtn.addEventListener("click", closeModal);
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
 
 const generateUserList = (data) => {
   let userListTemplate = "";
@@ -19,7 +59,7 @@ const generateUserList = (data) => {
           <img src="${el.avatar}" class="user-avatar">
         </div>
         <div class="user-body">
-          <p class="user-name" data-id=${el.id}>${el.name}<br>${el.surname}</p>
+          <p class="user-name" data-id=${el.id}>${el.name}<br>${el.surname} </p>
         </div>
       </div>
   `)
@@ -27,47 +67,7 @@ const generateUserList = (data) => {
   dataPanel.innerHTML = userListTemplate;
 };
 
-const openModal = () => {
-  modal.style.display = "block";
-};
-
-const closeModal = () => {
-  modal.style.display = "none";
-};
-
-openModalBtn.addEventListener("click", openModal);
-
-closeModalBtn.addEventListener("click", closeModal);
-
-window.addEventListener("click", function (event) {
-  if (event.target === modal) {
-    closeModal();
-  }
-});
-
 axios.get(INDEX_URL).then((res) => {
   const data = res.data.results;
   generateUserList(data);
-});
-
-dataPanel.addEventListener("click", function (event) {
-  if (event.target.matches(".user-name")) {
-    const id = event.target.dataset.id;
-
-    axios.get(SHOW_URL + id).then(function (res) {
-      const data = res.data;
-      console.log(res.data);
-      const userName = document.querySelector("#modal-user-name");
-      const userGender = document.querySelector("#modal-user-gender");
-      const userAge = document.querySelector("#modal-user-age");
-      const userEmail = document.querySelector("#modal-user-email");
-
-      userName.innerText = `${data.name} ${data.surname}`;
-      userGender.innerText = data.gender;
-      userAge.innerText = data.age;
-      userEmail.innerText = data.email;
-
-      openModal();
-    });
-  }
 });
