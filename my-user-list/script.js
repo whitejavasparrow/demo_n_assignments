7de217e93d7292253930f05ff3ddfbdc8be745b7
Pei-Yi Chen <peiyijessychen@gmail.com>
2023-03-08 11:52:40 +0800

    my-user-list

---
+++ my-user-list/script.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const BASE_URL = "https://user-list.alphacamp.io";
+const INDEX_URL = BASE_URL + "/api/v1/users";
+const SHOW_URL = BASE_URL + "/api/v1/users/";
+
+const dataPanel = document.querySelector("#data-panel");
+const modal = document.querySelector("#user-modal");
+const closeModalBtn = document.querySelector(".close-btn");
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+
+let data = [];
+
+const openModal = () => {
+  modal.style.display = "block";
+};
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+
+dataPanel.addEventListener("click", function (event) {
+  if (
+    event.target.matches(".user-name") ||
+    event.target.matches(".user-avatar")
+  ) {
+    const id = event.target.dataset.id; //
+
+    axios.get(SHOW_URL + id).then(function (res) {
+      data = res.data;
+      console.log(res.data);
+      const userName = document.querySelector("#modal-user-name");
+      const userAge = document.querySelector("#modal-user-age");
+      const userEmail = document.querySelector("#modal-user-email");
+
+      userName.innerText = `${data.name} ${data.surname} (${
+        data.gender === "female" ? "She / her / Ms." : "He / his / Mr."
+      })`;
+      userAge.innerText = `Birthday: ${data.birthday} (${data.age})`;
+      userEmail.innerText = `based in ${data.region} | email: ${data.email}`;
+
+      openModal();
+    });
+  }
+});
+
+closeModalBtn.addEventListener("click", closeModal);
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+const generateUserList = (data) => {
+  let userListTemplate = "";
+  data.map(
+    (el) =>
+      (userListTemplate += `
+      <div class="user">
+        <div class="user-title">
+          <img src="${el.avatar}" class="user-avatar">
+        </div>
+        <div class="user-body">
+          <p class="user-name" data-id=${el.id}>${el.name}<br>${el.surname} </p>
+        </div>
+      </div>
+  `)
+  );
+  dataPanel.innerHTML = userListTemplate;
+};
+
+axios.get(INDEX_URL).then((res) => {
+  const data = res.data.results;
+  generateUserList(data);
+});
+
+// searchbar
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const keyword = searchInput.value.trim().toLowerCase();
+
+  if (!keyword.length) {
+    return alert("請輸入有效字串！");
+  }
+
+  console.log(data);
+});
