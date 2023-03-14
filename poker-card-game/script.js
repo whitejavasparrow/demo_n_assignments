27c0079533cb57ff377860f6a5f2700605b0ac22
Pei-Yi Chen <peiyijessychen@gmail.com>
2023-03-14 14:50:28 +0800

    not working :(

--- poker-card-game/script.js
+++ poker-card-game/script.js
@@ -1,73 +1,107 @@
-// 初始變數
-const list = document.querySelector("#my-todo");
-const doneList = document.querySelector("#my-done");
-const addBtn = document.querySelector("#add-btn");
-const input = document.querySelector("#new-todo");
+"use strict";
 
-// 資料
-const todos = [
-  "Hit the gym",
-  "Read a book",
-  "Buy eggs",
-  "Organize office",
-  "Pay bills",
-];
+const backgroundOriginal =
+  "url(https://images.unsplash.com/photo-1584282000185-87fb204a83d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=972&q=80)";
 
-for (const todo of todos) {
-  addItem(todo);
-}
+const backgroundFlipped =
+  "url(https://images.unsplash.com/photo-1531131141161-ecdfb1858dd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)";
 
-// 函式
-function addItem(text) {
-  input.value = "";
+const dataPanel = document.querySelector("#data-panel");
 
-  const newItem = document.createElement("li");
-  newItem.innerHTML = `
-    <label for="todo">${text}</label>
-    <i class="delete fa fa-trash"></i>
+let currentTargets = [];
+let currentNums = [];
+let currentSuits = [];
+
+const generateCards = () => {
+  const suits = ["♠", "❤︎", "♦", "♣"];
+  let cards = [];
+  for (const suit of suits) {
+    let cnt = 0;
+    while (cnt < 13) {
+      cards.push(`${suit} ${cnt + 1}`);
+      cnt++;
+    }
+  }
+  return cards;
+};
+
+const shuffleCards = (cards) => {
+  return cards.sort((a, b) => 0.5 - Math.random());
+  // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+};
+
+function generateHTML(cards) {
+  let rawHTML = "";
+  cards.forEach((el) => {
+    const [cardSuit, cardNum] = el.split(" ");
+    const cardColor = ["❤︎", "♦"].includes(el.split(" ")[0]) ? "red" : "black";
+    rawHTML += `
+    <div class="card">
+      <div class="card-content">
+        <span class="card-num">${cardNum}</span>
+        <span class="card-suit suit-color-${cardColor}">${cardSuit}</span>
+        <span class="card-num-reversed">${cardNum}</span>
+      </div>
+    </div>
   `;
-  list.appendChild(newItem);
+  });
+  dataPanel.innerHTML = rawHTML;
 }
 
-// Create
-addBtn.addEventListener("click", function () {
-  const inputValue = input.value;
+const cards = generateHTML(shuffleCards(generateCards()));
 
-  if (inputValue.trim().length > 0) {
-    addItem(inputValue);
+const showCard = (target) => {
+  target.style.backgroundImage = backgroundFlipped;
+  target.children[0].style.display = "block";
+};
+
+const hideCard = (target) => {
+  target.style.backgroundImage = backgroundOriginal;
+  target.children[0].style.display = "none";
+};
+
+const addData = (target) => {
+  if (currentTargets.length < 2 + 1) {
+    currentTargets.push(target);
+    currentNums.push(+target.children[0].children[0].innerText);
+    currentSuits.push(target.children[0].children[1].innerText);
   } else {
-    alert("The addition might be an empty one");
+    currentTargets = [target];
+    currentNums = [+target.children[0].children[0].innerText];
+    currentSuits = [target.children[0].children[1].innerText];
   }
-});
+};
 
-input.addEventListener("keyup", function (event) {
-  const inputValue = input.value;
+const cleanData = () => {
+  currentTargets = [];
+  currentNums = [];
+  currentSuits = [];
+};
 
-  if (event.key === "Enter" && inputValue.trim().length > 0) {
-    addItem(inputValue);
-  }
-});
+let clickCnt = 0;
+dataPanel.addEventListener("click", (event) => {
+  if (event.target.matches(".card")) {
+    if (clickCnt === 0) {
+      const target = event.target;
+      addData(target);
+      showCard(target);
+      clickCnt++;
+    } else if (clickCnt === 1) {
+      const target = event.target;
+      addData(target);
+      showCard(target);
 
-// Delete and check
-list.addEventListener("click", function (event) {
-  const target = event.target;
-
-  if (target.classList.contains("delete")) {
-    const parentElement = target.parentElement;
-    parentElement.remove();
-  } else if (target.tagName === "LABEL") {
-    // why capitalized?
-    target.classList.toggle("checked");
-    const parentElement = target.parentElement;
-    doneList.appendChild(parentElement);
-  }
-});
-
-doneList.addEventListener("click", function (event) {
-  const target = event.target;
-
-  if (target.classList.contains("delete")) {
-    const parentElement = target.parentElement;
-    parentElement.remove();
+      if (
+        currentNums[0] === currentNums[1] &&
+        currentSuits[0] !== currentSuits[1]
+      ) {
+        console.log("This is a match!");
+      } else {
+        console.log(currentNums);
+        hideCard(currentTargets[0]);
+        hideCard(currentTargets[1]);
+        cleanData();
+      }
+    }
   }
 });
