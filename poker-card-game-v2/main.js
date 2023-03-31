55edc90468fc2950afedf97d01bf510109a41ce3
Pei-Yi Chen <peiyijessychen@gmail.com>
2023-03-31 14:19:57 +0800

    mvc

--- poker-card-game-v2/main.js
+++ poker-card-game-v2/main.js
@@ -1,3 +1,10 @@
+const GAME_STATE = {
+  FirstCardAwaits: "FirstCardAwaits",
+  SecondCardAwaits: "SecondCardAwaits",
+  CardsMatchFailed: "CardsMatchFailed",
+  CardsMatched: "CardsMatched",
+  GameFinished: "GameFinished",
+};
 const Symbols = [
   "https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png", // 黑桃
   "https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png", // 愛心
@@ -6,14 +13,16 @@ const Symbols = [
 ];
 const view = {
   getCardElement(index) {
+    return `<div data-index="${index}" class="card back"></div>`;
+  },
+  getCardContent(index) {
     const number = this.transformNumber((index % 13) + 1);
     const symbol = Symbols[Math.floor(index / 13)];
     return `
-      <div class="card">
-        <p>${number}</p>
-        <img src="${symbol}" />
-        <p>${number}</p>
-      </div>`;
+      <p>${number}</p>
+      <img src="${symbol}" />
+      <p>${number}</p>
+    `;
   },
   transformNumber(number) {
     switch (number) {
@@ -29,11 +38,97 @@ const view = {
         return number;
     }
   },
-  displayCards() {
+  displayCards(indexes) {
     const rootElement = document.querySelector("#cards");
-    rootElement.innerHTML = Array.from(Array(52).keys())
+    rootElement.innerHTML = indexes
       .map((index) => this.getCardElement(index))
       .join("");
   },
+  flipCard(card) {
+    if (card.classList.contains("back")) {
+      // 回傳正面
+      card.classList.remove("back");
+      card.innerHTML = this.getCardContent(Number(card.dataset.index));
+      return;
+    }
+    // 回傳背面
+    card.classList.add("back");
+    card.innerHTML = null;
+  },
+  pairCard(card) {
+    card.classList.add("paired");
+  },
 };
-view.displayCards();
+const model = {
+  revealedCards: [],
+  isRevealedCardsMatched() {
+    return (
+      this.revealedCards[0].dataset.index % 13 ===
+      this.revealedCards[1].dataset.index % 13
+    );
+  },
+};
+const controller = {
+  currentState: GAME_STATE.FirstCardAwaits,
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52));
+  },
+  dispatchCardAction(card) {
+    if (!card.classList.contains("back")) {
+      return;
+    }
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwaits:
+        view.flipCard(card);
+        model.revealedCards.push(card);
+        this.currentState = GAME_STATE.SecondCardAwaits;
+        break;
+      case GAME_STATE.SecondCardAwaits:
+        view.flipCard(card);
+        model.revealedCards.push(card);
+        // 判斷配對是否成功
+        if (model.isRevealedCardsMatched()) {
+          // 配對成功
+          this.currentState = GAME_STATE.CardsMatched;
+          view.pairCard(model.revealedCards[0]);
+          view.pairCard(model.revealedCards[1]);
+          model.revealedCards = [];
+          this.currentState = GAME_STATE.FirstCardAwaits;
+        } else {
+          // 配對失敗
+          this.currentState = GAME_STATE.CardsMatchFailed;
+          setTimeout(() => {
+            view.flipCard(model.revealedCards[0]);
+            view.flipCard(model.revealedCards[1]);
+            model.revealedCards = [];
+            this.currentState = GAME_STATE.FirstCardAwaits;
+          }, 1000);
+        }
+        break;
+    }
+    console.log("this.currentState", this.currentState);
+    console.log(
+      "revealedCards",
+      model.revealedCards.map((card) => card.dataset.index)
+    );
+  },
+};
+const utility = {
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys());
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1));
+      [number[index], number[randomIndex]] = [
+        number[randomIndex],
+        number[index],
+      ];
+    }
+    return number;
+  },
+};
+controller.generateCards();
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", (event) => {
+    controller.dispatchCardAction(card);
+  });
+});
